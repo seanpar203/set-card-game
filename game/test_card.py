89acc4e353411a637ba39b3f7c99a32221b22860
Sean Parsons <seanpatrick2013@gmail.com>
2024-06-01 08:14:45 -0400

    Added some more testing for the Set class

--- game/test_card.py
+++ game/test_card.py
@@ -1,5 +1,6 @@
 from .card import (
     Card,
+    Set,
     create_sets,
     create_card_deck,
 )
@@ -14,6 +15,69 @@ def test_Card_uid():
 def test_Card__str__():
     card = Card(Color.RED, Shade.FULL, Shape.CIRCLE, Number.ONE)
     assert str(card) == "1111"
+
+
+def test_Set__update_key():
+    card = Card(Color.RED, Shade.FULL, Shape.CIRCLE, Number.ONE)
+    set = Set()
+    set.add(card)
+
+    assert set.key == "1111"
+
+    set.add(card)
+
+    assert set.key == "1111-1111"
+
+def test_Set__iter__():
+    card = Card(Color.RED, Shade.FULL, Shape.CIRCLE, Number.ONE)
+    set = Set()
+    set.add(card)
+    
+    assert next(iter(set)) == card
+
+
+def test_Set__len__():
+    card = Card(Color.RED, Shade.FULL, Shape.CIRCLE, Number.ONE)
+    set = Set()
+    set.add(card)
+    
+    assert len(set) == 1
+
+def test_Set_add():
+    card = Card(Color.RED, Shade.FULL, Shape.CIRCLE, Number.ONE)
+    set = Set()
+    set.add(card)
+    
+    assert set.cards[0] == card
+
+
+def test_Set_is_set():
+
+    # Test True case
+    card_one = Card(Color.RED, Shade.FULL, Shape.CIRCLE, Number.ONE)
+    card_two = Card(Color.RED, Shade.HALF, Shape.CIRCLE, Number.TWO)
+    card_three = Card(Color.RED, Shade.EMPTY, Shape.CIRCLE, Number.THREE)
+
+    set = Set()
+
+    set.add(card_one)
+    set.add(card_two)
+    set.add(card_three)
+
+    assert set.is_set() == True
+
+    # Test False case
+    card_one = Card(Color.RED, Shade.FULL, Shape.CIRCLE, Number.ONE)
+    card_two = Card(Color.BLUE, Shade.HALF, Shape.CIRCLE, Number.TWO)
+    card_three = Card(Color.BLUE, Shade.EMPTY, Shape.CIRCLE, Number.THREE)
+
+    set = Set()
+
+    set.add(card_one)
+    set.add(card_two)
+    set.add(card_three)
+
+    assert set.is_set() == False
 
 
 def test_create_card_deck():
